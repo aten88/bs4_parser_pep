217e52a24bb5ed53504bf3c81c5bf0c561f71c54
aten88 <virtuoso_ten@mail.ru>
2023-11-25 12:08:17 +0300

    Подготовил проект к работе по 2 м пунктам

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+BASE_DIR = Path(__file__).parent
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
---
+++ src/main.py
@@ -0,0 +1,121 @@
+import re
+from urllib.parse import urljoin
+import logging
+
+import requests_cache
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from constants import BASE_DIR, MAIN_DOC_URL
+from configs import configure_argument_parser, configure_logging
+from outputs import control_output
+from utils import get_response, find_tag
+
+
+def whats_new(session):
+    """ Парсер обновления документации по Python. """
+    what_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
+    response = get_response(session, what_new_url)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, features='lxml')
+    main_div = find_tag(soup, 'section', attrs={'id': 'what-s-new-in-python'})
+    div_with_ul = find_tag(main_div, 'div', attrs={'class': 'toctree-wrapper'})
+    section_by_python = div_with_ul.find_all(
+        'li', attrs={'class': 'toctree-l1'}
+    )
+    results = [('Ссылка на статью', 'Заголовок', 'Редактор, автор')]
+    for section in tqdm(section_by_python):
+        version_a_tag = find_tag(section, 'a')
+        href = version_a_tag['href']
+        version_link = urljoin(what_new_url, href)
+        response = get_response(session, version_link)
+        if response is None:
+            continue
+        soup = BeautifulSoup(response.text, features='lxml')
+        h1 = find_tag(soup, 'h1').text
+        dl = find_tag(soup, 'dl').text
+        dl.replace('\n', ' ')
+        results.append((version_link, h1, dl))
+
+    return results
+
+
+def latest_versions(session):
+    """ Парсер версий Python и их состояния. """
+    response = get_response(session, MAIN_DOC_URL)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, 'lxml')
+    sidebar = find_tag(soup, 'div', {'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+    for ul in ul_tags:
+        if 'All version' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+
+    results = [('Ссылка на документацию', 'Версия', 'Статус')]
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    for a_tag in a_tags:
+        link = a_tag['href']
+        text_match = re.search(pattern, a_tag.text)
+        if text_match is not None:
+            version, status = text_match.groups()
+        else:
+            version, status = a_tag.text, ''
+        results.append((link, version, status))
+
+    return results
+
+
+def download(session):
+    """ Парсер архива Python. """
+    download_urls = urljoin(MAIN_DOC_URL, 'download.html')
+    response = get_response(session, download_urls)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, 'lxml')
+    main_tag = find_tag(soup, 'div', {'role': 'main'})
+    table_tag = find_tag(main_tag, 'table', {'class': 'docutils'})
+    pdf_a4_tag = find_tag(
+        table_tag, 'a', {'href': re.compile(r'.+pdf-a4\.zip$')}
+    )
+    pdf_a4_link = pdf_a4_tag['href']
+    archive_url = urljoin(download_urls, pdf_a4_link)
+    filename = archive_url.split('/')[-1]
+
+    downloads_dir = BASE_DIR / 'downloads'
+    downloads_dir.mkdir(exist_ok=True)
+    archive_path = downloads_dir / filename
+    response = session.get(archive_url)
+    with open(archive_path, 'wb') as file:
+        file.write(response.content)
+    logging.info(f'Архив был загружен и сохранён: {archive_path}')
+
+
+MODE_TO_FUNCTION = {
+    'whats-new': whats_new,
+    'latest-versions': latest_versions,
+    'download': download,
+}
+
+
+def main():
+    configure_logging()
+    logging.info('Парсер запущен!')
+
+    arg_parser = configure_argument_parser(MODE_TO_FUNCTION.keys())
+    args = arg_parser.parse_args()
+    logging.info(f'Аргументы командной строки: {args}')
+    session = requests_cache.CachedSession()
+    if args.clear_cache:
+        session.cache.clear()
+    parser_mode = args.mode
+    results = MODE_TO_FUNCTION[parser_mode](session)
+    if results is not None:
+        control_output(results, args)
+    logging.info('Парсер завершил работу. ')
+
+
+if __name__ == "__main__":
+    main()
