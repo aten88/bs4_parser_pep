05d660bd60bf1b771839e205d74210c45aff4974
Алексей Тен <virtuoso_ten@mail.ru>
2023-11-27 17:29:22 +0300

    Удалось получить количество документов PEP

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import requests
+import requests_cache
 from bs4 import BeautifulSoup
 
 from constants import MAIN_DOC_URL
 
-
-response = requests.get(MAIN_DOC_URL)
+session = requests_cache.CachedSession()
+response = session.get(MAIN_DOC_URL)
+response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+
+num_index_section = soup.find('section', attrs={'id': 'numerical-index'})
+peps_body_table = num_index_section.find('tbody')
+peps_a = peps_body_table.find_all('tr')
+count = 0
+for pep_a in peps_a:
+    name_pep = pep_a.find('a')
+    print(name_pep)
+    count += 1
+print(count)
