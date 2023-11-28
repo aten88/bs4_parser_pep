e2f93445aa102cb0ed58c81e706b7960c492833e
aten88 <virtuoso_ten@mail.ru>
2023-11-28 19:43:00 +0300

    Выводит на печать 2 словаря

--- main.py
+++ main.py
@@ -11,34 +11,40 @@ soup = BeautifulSoup(response.text, 'lxml')
 
 num_index_section = soup.find('section', attrs={'id': 'numerical-index'})
 peps_body_table = num_index_section.find('tbody')
-peps_a = peps_body_table.find_all('tr')
-# print(peps_a)
+peps_tr = peps_body_table.find_all('tr')
+
 count_peps_doc = 0  # Счетчик кол-ва PEP
-for pep_a in peps_a:
+
+exterior_list_pep = []
+interior_list_pep = []
+
+for pep_a in peps_tr:
     name_pep = pep_a.find('a')
+    title_name_general = name_pep['title']  # Название PEP из внешней таблицы
+
     td_pep = pep_a.find('td')
     abbr_pep = td_pep.find('abbr')
     status_pep_general = abbr_pep['title'].split(", ", 1)[1]
     # Статус PEP из общей таблицы
-    title_name_general = name_pep['title']  # Название PEP из внешней таблицы
-    print(f'{status_pep_general} | во внешней таблице')
+
+    dict_pep_ext = {status_pep_general: title_name_general}
+    exterior_list_pep.append(dict_pep_ext)
+
     href = name_pep['href']
     pep_link = urljoin(PEPS_DOC_URL, href)
     count_peps_doc += 1
-    response = session.get(pep_link)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'lxml')
+    response1 = session.get(pep_link)
+    response1.encoding = 'utf-8'
+    soup = BeautifulSoup(response1.text, 'lxml')
     in_card_pep_name = soup.find('title').text.replace(
         " | peps.python.org", ""
     )  # Спарсенное наименование PEP внутри карточки
-    status_in_card_pep = soup.find('abbr').text  # Спарсенный статус внутри PEP
-    print(f'{status_in_card_pep} | внутри карточки')
-    no_match = 0
-    if status_pep_general != status_in_card_pep:
-        no_match += 1
-        print('Найдено отличие. ')
-    print('---------------------------------------------------------------')
 
-print('----------------------------')
-print(count_peps_doc)
-print(no_match)
+    status_in_card_pep = soup.find('abbr').text  # Спарсенный статус внутри PEP
+
+    if status_pep_general != status_in_card_pep:
+        print('Найдено отличие. ')
+    dict_pep_int = {status_in_card_pep: in_card_pep_name}
+    interior_list_pep.append(dict_pep_int)
+print(exterior_list_pep)
+print(interior_list_pep)
