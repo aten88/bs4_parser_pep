7a37bef654d173c9ae545e2b0b51f584fefd20f8
Алексей Тен <virtuoso_ten@mail.ru>
2023-11-30 22:18:42 +0300

    Сделал обработку ошибок, остались тесты

--- utils.py
+++ utils.py
@@ -1,21 +1,31 @@
+import logging
+
 from constants import EXPECTED_STATUS
+from configs import configure_logging
 
 
 def compare_statuses(combined_list):
     """ Метод проверки соответствия статусов PEP. """
+    configure_logging()
     updated_list = []
     for item in combined_list:
         key, value = list(item.items())[0]
 
         expected_status = EXPECTED_STATUS.get(key)
         if expected_status is None:
-            print(f"Ошибка: Неожиданный статус '{key}' для '{value}' в списке")
+            logging.error(
+                f'\n'
+                f'Статус в карточке: {key}'
+                f'Ожидаемые статусы: {value}'
+            )
             continue
         if value in expected_status:
             updated_list.append(value)
         else:
-            print(
-                f"Ошибка: Несоответствие статуса для '{value}'. "
-                f"Ожидалось: {expected_status}, получено: '{value}'"
+            logging.error(
+                f'\n'
+                f'Несовпадающие статусы:\n'
+                f'Статус в карточке: {value}\n'
+                f'Ожидаемые статусы: {expected_status}'
             )
     return updated_list
